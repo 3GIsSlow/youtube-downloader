770af43ddd57dbadead2f64075a31e9cf13c5190
3GIsSlow <brettab3@hotmail.com>
2021-01-16 09:54:58 -0500

    Changed file structure

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, Brett!</h1>');
+    res.send("Test");
 });
 
 app.listen(port, () => {
